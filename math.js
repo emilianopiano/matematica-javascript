2604d5d24f643a71b38506501277b2d7d0ac18e0
Eduar Galeano <eduardestiveng@gmail.com>
2023-02-19 16:58:08 -0500

    Se agrega una calculadora de descuento

--- math.js
+++ math.js
@@ -1,3 +1,36 @@
+const valor = document.querySelector('#valor');
+const descuento = document.querySelector('#descuento');
+const resultado = document.querySelector('.resul input');
+const total = document.querySelector('#resulta');
+ 
+
+
+function calcularDescuento(){
+
+    
+
+    const des = descuento.value;
+    const val = valor.value;
+    const res = (des/100)*val;
+    const restotal = val-res;
+
+    
+
+    console.log(res);
+    console.log(restotal);
+    total.innerHTML = "resultado" + " " + restotal;
+
+
+    
+
+}
+
+
+console.log(valor.value);
+
+
+
+
 
 console.group('Cuadrado')
 
@@ -100,3 +133,47 @@ function calcularCirculo(radio){
 
 
 console.groupEnd('Circle')
+
+console.group('Escaleno')
+
+
+
+
+function alturaEscaleno(a, b, c){
+
+    /* S = semiperimetro = a+b+c/2*/
+    
+    if (a===b || b===c || c===a ){
+
+        console.warn('NO es un triangulo Escaleno')
+        
+    }
+
+    else {
+
+        const S = (a+b+c)/2;
+
+        return (2/a)*(Math.sqrt(S*(S-a)*(S-b)*(S-c)));
+
+        
+
+ }
+    
+    
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+console.groupEnd('Escaleno')
